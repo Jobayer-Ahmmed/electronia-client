5c9a23918166ca09e99a5a17addd6aabbfc01656
%Sa2323ma <jobayer.ahmmed.jas@gmail.com>
2024-02-19 17:45:41 +0600

    add: login

--- src/routes/MainRoute.jsx
+++ src/routes/MainRoute.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
 import Home from "../pages/home/home/Home";
 import Register from "../pages/register/Register";
+import Login from "../pages/login/Login";
 
 
 const router = createBrowserRouter([{
@@ -15,6 +16,10 @@ const router = createBrowserRouter([{
         {
             path:"/register",
             element:<Register/>
+        },
+        {
+            path:"/login",
+            element:<Login/>
         }
     ]
 }])
