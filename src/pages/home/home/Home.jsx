74dc2b5584254ec22e306ebaf9c0204edfa55964
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-02-21 18:37:47 +0600

    add: laptop

--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,6 +1,7 @@
 import Discount from "../discount/Discount";
 import Slider from "../hero/Slider";
 import KitchenGadgets from "../kitchenGadgets/KitchenGadgets";
+import Laptop from "../laptop/Laptop";
 import TechTrends from "../techTrends/TechTrends";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
       <div className="px-5 md:px-10 lg:px-20">
         <Discount />
         <TechTrends />
+        <Laptop/>
         <KitchenGadgets/>
       </div>
     </div>
