e89f80ff214ba756c0d459197b62e24055244cfc
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-02-27 23:33:29 +0600

    update: secondaryBanner

--- src/pages/home/secondaryHome/secondaryBanner/SecondaryBanner.jsx
+++ src/pages/home/secondaryHome/secondaryBanner/SecondaryBanner.jsx
@@ -26,12 +26,12 @@ const SecondaryBanner=()=> {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide><Link to="/phone"><img src={phone} alt=''/></Link></SwiperSlide>
-        <SwiperSlide><Link to="/laptop"><img src={laptop} alt=''/></Link></SwiperSlide>
-        <SwiperSlide><Link to="/ac"><img src={ac} alt=''/></Link></SwiperSlide>
-        <SwiperSlide><Link to="/tv"><img src={tv} alt=''/></Link></SwiperSlide>
-        <SwiperSlide><Link to="/kitchen"><img src={kitchen} alt=''/></Link></SwiperSlide>
-        <SwiperSlide><Link to="/refrigerator"><img src={refrigerator} alt=''/></Link></SwiperSlide>
+        <SwiperSlide><Link to="/phone"><img src={phone} alt=''/> <p className='text-cardTextColor font-bold'>Phone</p></Link></SwiperSlide>
+        <SwiperSlide><Link to="/laptop"><img src={laptop} alt=''/><p className='text-cardTextColor font-bold'>laptop</p></Link></SwiperSlide>
+        <SwiperSlide><Link to="/ac"><img src={ac} alt=''/><p className='text-cardTextColor font-bold'>AC</p></Link></SwiperSlide>
+        <SwiperSlide><Link to="/tv"><img src={tv} alt=''/><p className='text-cardTextColor font-bold'>TV & Monitor</p></Link></SwiperSlide>
+        <SwiperSlide><Link to="/kitchen"><img src={kitchen} alt=''/><p className='text-cardTextColor font-bold'>Kitchen Gadet</p></Link></SwiperSlide>
+        <SwiperSlide><Link to="/refrigerator"><img src={refrigerator} alt=''/><p className='text-cardTextColor font-bold'>Refrigerator</p></Link></SwiperSlide>
       </Swiper>
     </>
   );
